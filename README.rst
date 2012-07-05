b5bc644f54d2e413f4bad53fa416112439f8c8ba
Kelsey Jordahl <kjordahl@alum.mit.edu>
2012-07-05 08:50:48 -0400

    Add license and README

---
+++ README.rst
@@ -0,0 +1,22 @@
+Subwoofer modeling
+==================
+This is a simple tool for modeling the frequency response of a subwoofer enclosure with a given driver.  By interactively adjusting the volume of the enclosure, the user can immediately see the change in frequency response.  The equations used for modeling the behavior are based on those at http://www.diysubwoofers.org.
+
+Files
+-----
+* README.rst: This file
+* COPYING: GPL v3 license
+* sub_model.py: The main Python program file. Run this to start the model.
+* params.cfg: Driver parameters. Edit this file to add new drivers.
+* sub_view.enaml: Enaml GUI file
+* frequency_plot.py: Chaco plotting tools
+
+Requirements
+------------
+* Python 2.6 or 2.7
+* NumPy
+* Traits
+* Enaml
+* Chaco
+
+All of the above requirements are met by the Enthought Python Distribution (EPD), including EPD Free.
--- sub_model.py
+++ sub_model.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Subwoofer modeling tool
 
+Author: Kelsey Jordahl
+Version: alpha
+Copyright: Kelsey Jordahl 2012
+License: GPLv3
+Time-stamp: <Thu Jul  5 08:50:27 EDT 2012>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.  A copy of the GPL
+    version 3 license can be found in the file COPYING or at
+    <http://www.gnu.org/licenses/>.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+"""
 import ConfigParser
 import numpy as np
 import matplotlib.pyplot as plt
